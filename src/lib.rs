418e3c54e33bb23f37ee8d47997bf604c4241c7f
Luca Barbato <lu_zero@gentoo.org>
2022-08-26 10:19:33 +0200

    doc: Document servient

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,8 @@
 //! Provides all the building blocks to serve [Web Of Things](https://www.w3.org/WoT/) Things.
 
 pub mod advertise;
+#[doc(hidden)]
 pub mod hlist;
 pub mod servient;
+
+pub use servient::Servient;
